a0d3aa48c38ee2a68cfae51ca1526f3c4f492ba3
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-03-17 11:21:52 -0600

    Displays ESPN.com

--- WebViewDemo/ViewController.m
+++ WebViewDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface ViewController ()
+
+@property (strong,nonatomic) WKWebView *webView;
 
 @end
 
@@ -17,11 +20,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.webView = [[WKWebView alloc]initWithFrame:self.view.frame];
+
+       [self.view addSubview:self.webView];
+     [self configureView];
+ 
+}
+
+- (void)configureView {
+    
+    NSURL *url = [NSURL URLWithString:@"http://espn.go.com/"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    
 }
+
+
 
 @end
